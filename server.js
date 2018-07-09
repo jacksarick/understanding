51168e70bfc15f10553c7e693e3698e68f5168a7
jacksarick <jacksarick@gmail.com>
2018-07-08 21:59:19 -0600

    Adding file upload

--- server.js
+++ server.js
@@ -1,38 +1,45 @@
-// set up
+// setup the server reqs
+var fs = require('fs');
 var express = require('express');
 var app = express();
-var config = require('./config/config.js')
+var config = require('./config/config.js');
 var port = process.env.PORT || config.port;
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
 
-var db
+// establish db connection
+const MongoClient = require('mongodb').MongoClient;
+var db;
 
 MongoClient.connect(config.db.url, (err, client) => {
-	if (err) return console.log(err)
+	if (err) return console.log(err);
 	db = client.db(config.db.name);
 	app.listen(port, () => {
 		console.log("App listening on port " + port);
 	})
 })
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(express.static('public'))
+// set app guidelines
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use('/uploads', express.static(__dirname +'/uploads'));
+app.use(multer({dest: './uploads/'}));
 
+// route requests around
 app.get('/', (req, res) => {
 	db.collection('memes').find().toArray((err, result) => {
-		if (err) return console.log(err)
-		res.render('index.ejs', {memes: result})
+		if (err) return console.log(err);
+		res.render('index.ejs', {memes: result});
 	})
 })
 
 app.post('/memes', (req, res) => {
 	db.collection('memes').save(req.body, (err, result) => {
-		if (err) return console.log(err)
-		console.log('saved to database')
-		res.redirect('/')
+		if (err) return console.log(err);
+		console.log('saved to database');
+		res.redirect('/');
 	})
 })
+
