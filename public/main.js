413a206ed8313808938228c9e50347fc4c8b331e
Jack's Server <jacksarick@gmail.com>
2018-07-09 00:42:51 +0000

    Save Mon Jul  9 00:42:51 UTC 2018: 'Prettied the whole thing up'

--- public/main.js
+++ public/main.js
@@ -1,3 +1,8 @@
 window.onload = function(){
 	document.getElementById('date_input').valueAsDate = new Date();
 }
+
+toggle_modal = function() {
+	console.log("toggle")
+	document.getElementById('creation-modal').classList.toggle("is-active");
+}
